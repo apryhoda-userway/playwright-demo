d2bdb85306d030abd49577d065813c68c5805955
Danylo Varfolomieiev <varfdaniil@gmail.com>
2023-10-23 10:06:37 +0300

    disable several browsers to test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig, devices } from '@playwright/test'
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-require('dotenv').config()
+require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -62,25 +62,25 @@ export default defineConfig({
             use: { ...devices['Desktop Chrome'] },
         },
 
-        {
-            name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
-        },
+        // {
+        //     name: 'firefox',
+        //     use: { ...devices['Desktop Firefox'] },
+        // },
 
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
-        },
+        // {
+        //     name: 'webkit',
+        //     use: { ...devices['Desktop Safari'] },
+        // },
 
         /* Test against mobile viewports. */
-        {
-            name: 'Mobile Chrome',
-            use: { ...devices['Pixel 5'] },
-        },
-        {
-            name: 'Mobile Safari',
-            use: { ...devices['iPhone 12'] },
-        },
+        //     {
+        //         name: 'Mobile Chrome',
+        //         use: { ...devices['Pixel 5'] },
+        //     },
+        //     {
+        //         name: 'Mobile Safari',
+        //         use: { ...devices['iPhone 12'] },
+        //     },
     ],
     outputDir: './output/test-results/',
 
@@ -90,4 +90,4 @@ export default defineConfig({
     //   url: 'http://127.0.0.1:3000',
     //   reuseExistingServer: !process.env.CI,
     // },
-})
+});
